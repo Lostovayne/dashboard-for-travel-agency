5fdb62104be5ef26e0e383d33f6b5adab10c9cd7
LostoVayne <moises.barrera.dev@gmail.com>
2025-04-27 23:27:37 -0400

    feat(components): add MobileSidebar and NavItems components
    Introduce new components MobileSidebar and NavItems to enhance the admin layout. Added utility functions in utils.ts for date formatting and markdown parsing. Updated dependencies and improved import syntax in root.tsx. Adjusted Vite config for better handling of external packages.

---
+++ app/components/NavItems.tsx
@@ -0,0 +1,65 @@
+import { cn } from "lib/utils";
+import { Link, NavLink } from "react-router";
+import { sidebarItems } from "~/constants";
+
+const NavItems = () => {
+  const user = {
+    name: "John Doe",
+    email: "contact@john.com",
+    imageUrl: "/assets/images/david.webp",
+  };
+
+  return (
+    <section className="nav-items">
+      <Link to="/" className="link-logo">
+        <img src="/assets/icons/logo.svg" alt="logo" className="size-[30px]" />
+        <h1>Tourvisto</h1>
+      </Link>
+
+      <div className="container">
+        <nav>
+          {sidebarItems.map(({ id, href, icon, label }) => (
+            <NavLink key={id} to={href}>
+              {({ isActive }: { isActive: boolean }) => (
+                <div
+                  className={cn("group nav-item transition-colors", {
+                    "bg-primary-100 !text-white": isActive,
+                  })}
+                >
+                  <img
+                    src={icon}
+                    alt={label}
+                    className={`group-hover:brightness-0 size-0 group-hover:invert ${
+                      isActive ? "brightness-0 invert" : "text-dark-200"
+                    }`}
+                  />
+                  {label}
+                </div>
+              )}
+            </NavLink>
+          ))}
+        </nav>
+
+        <footer className="nav-footer">
+          <img
+            src={user?.imageUrl || "/assets/images/david.webp"}
+            alt={user.name || "John Doe"}
+          />
+          <article>
+            <h2>{user?.name || "John Doe"}</h2>
+            <p>{user?.email || "jhon@doe.com"}</p>
+          </article>
+          <button
+            onClick={() => {
+              console.log("logout");
+            }}
+            className="cursor-pointer"
+          >
+            <img src="/assets/icons/logout.svg" alt="logout" className="size-6" />
+          </button>
+        </footer>
+      </div>
+    </section>
+  );
+};
+export default NavItems;
